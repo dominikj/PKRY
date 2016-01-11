ada7e1380237c0ef1403c77aa89496790f3f7504
Dominik Januszewicz <Dominik.Januszewicz.PL@ieee.org>
2016-01-11 02:38:36 +0100

    sprzątanie4

--- gui/gui.cpp
+++
@@ -1,9 +0,0 @@
-#include "gui.h"
-
-void GUI::uruchom(){
-    _logowanie.show();
-    //jak bedzie uruchomiony nasłuch to w momencie przechwycenia info o koniecznosci wyboru zwyciezcy
-    //trzeba uruchomic okno z wyborem zwyciezcy
-    //i je obsluzyc, guzik w Oknie Głównym jest tylko do testów
-}
-
